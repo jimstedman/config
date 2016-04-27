35bf6922bcc800cb59409971ca62caaa772c149d
Jim Stedman <jimwst@gmail.com>
2016-04-27 14:12:41 -0600

    simplified

--- workstation.sh
+++ workstation.sh
@@ -4,7 +4,7 @@
 git clone --recursive https://github.com/jimstedman/prezto.git ~/.zprezto
 for rcfile in ~/.zprezto/runcoms/z*; do
     rcfilename=$(echo $rcfile |cut -d'/' -f 6)
-    ln -fs "$rcfile" "~/.${rcfilename:t}"
+    ln -fs $rcfile ~/.${rcfilename:t}
 done
 
 ./wd_install.sh
