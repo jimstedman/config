ad50a583f18c111bb645d325aebe056daa8c7438
JS <sol@localhost.js>
2016-05-25 08:10:10 -0600

    add fedora setup

--- run_first.sh
+++ run_first.sh
@@ -2,11 +2,11 @@
 # run from root to install needed packages using apt
 
 if [ "$(id -u)" != "0" ]; then
-    echo "This script installs needed packages using apt, please run as root."
+    echo "This script installs needed packages using apt-get, please run as root."
     2>&1
     exit 1
 fi
 
-apt-get install zsh tmux vim-nox
+apt-get -y install zsh tmux vim
 
-update-alternatives --set editor $(which vim-nox)
+update-alternatives --set editor $(which vim)
